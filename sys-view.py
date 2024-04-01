ab241cc81153bd9a68a71751e13c36fe05c1cccb
rasmnout <165410431+rasmnout@users.noreply.github.com>
2024-04-01 15:30:20 +0200

    Update sys-view.py

--- sys-view.py
+++ sys-view.py
@@ -1,12 +1,15 @@
 import subprocess
+import time
 
 def run():
     try:
-        with open("/rasmnout/sys/output.log", "+r") as f:
-            fg = f.read()
-            while True:
-                f.seek(0)
+        while True:
+            with open("/rasmnout/log/execute.log", "r") as f:
+                current_position = f.tell()  # Store current position
+                fg = f.read()
+                f.seek(current_position)  # Seek back to the stored position
                 fgg = f.read()
+                
                 if fgg == fg:
                     pass
                 else:
@@ -14,18 +17,19 @@ def run():
                     process = subprocess.Popen(fgg, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
                     output, error = process.communicate()
                     print(output.decode())
-                    status = "sucessfully"
+                    status = "successfully"
                     if error:
                         print("[PROBLEM] PROBLEM:")
                         print(error.decode())
                         status = "error"
-                    with open("/rasmnout/log/all-output.log","a") as f:
-                      f.write(output.decode())
-                    with open("/rasmnout/log/output.log","w") as f:
-                      f.write(output.decode())
-                    with open("/rasmnout/log/status.log","w") as f:
-                      f.write(status)
+                    with open("/rasmnout/log/all-output.log", "a") as f:
+                        f.write(output.decode())
+                    with open("/rasmnout/log/output.log", "w") as f:
+                        f.write(output.decode())
+                    with open("/rasmnout/log/status.log", "w") as f:
+                        f.write(status)
+            time.sleep(1)  # Add a short delay to avoid busy-waiting
     except FileNotFoundError:
-        print("[PROBLEM] error with file execute.rsmnt")
+        print("[PROBLEM] error with file execute.log")
 
 run()
