16d41491694ba271d8387c9ff780db1cfaa28636
rasmnout <165410431+rasmnout@users.noreply.github.com>
2024-04-11 08:56:54 +0200

    Delete sys-message.py

--- sys-message.py
+++
@@ -1,8 +0,0 @@
-from colorama import *
-
-def info(message=""):
-    return f"[{Fore.BLUE}INFO{Fore.RESET}] {message}"
-def problem(message=""):
-    return f"[{Fore.RED}PROBLEM{Fore.RESET}] {message}"
-def warning(message=""):
-    return f"[{Fore.YELLOW}WARNING{Fore.RESET}] {message}"
